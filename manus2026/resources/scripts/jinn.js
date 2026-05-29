9fccfd64e4ef320931506996509124b878b5911d
Holger Berg <36001578+ogierMontanus@users.noreply.github.com>
2026-05-29 15:44:23 +0200

    remained irrelevant subfolders

--- manus2026/resources/scripts/jinn.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('window-opened',
-e => {
-	const facs = e.detail.content.querySelector('pb-facsimile');
-
-	setTimeout(() => {
-		if(!facs.viewer) facs._initOpenSeadragon()
-	} , 4)
-});
